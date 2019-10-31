b1a691235a86f73c087613608bc63d4ebc0db291
wollbo <Hilding@n182-p16.eduroam.kth.se>
2019-10-31 16:14:17 +0100

    mer stuff

--- lab0.py
+++ lab0.py
@@ -19,7 +19,7 @@ n_states = len(states)
 p = np.zeros((n_states, n_states))
 
 ne_rel_inds = [(0, 0), (-1, 0), (0, 1), (1, 0), (0, -1)]
-
+neighbours = [None for _ in range(n_states)]
 for state in states:
     ne_states = []
     for ne_rel_ind in ne_rel_inds:
@@ -29,12 +29,18 @@ for state in states:
         if (n_rows > ne_ind[0] >= 0) and (n_cols > ne_ind[1] >= 0):
             if maze[ne_ind]:
                 ne_states.append(ne_ind)
+    neighbours[states.index(state)] = ne_states
 
     n_nes = len(ne_states)
 
     for ne in ne_states:
         p[states.index(state), states.index(ne)] = 1/n_nes
 
-print(np.sum(p, axis=1))
+rewards = [1/(0.1+np.linalg.norm(np.array([state[0]-5, state[1]-5]))) for state in states]
+
+decay = 0.9
+
+def value(states, rewards, val)
+    
 
 
