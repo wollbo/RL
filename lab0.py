fc38f1f2f81fd5453e05b39d3507d2158018890e
torstenmolitor <torstenmolitor@hotmail.com>
2019-11-03 18:03:17 +0100

    promising value/policy update algorithm

--- lab0.py
+++ lab0.py
@@ -17,22 +17,34 @@ class State:
         self.index = index
         self.neighbours = []
         self.value = 0
-        self.policy = None  # index of neighbour to go to!
+        self.next_value = 0
+        self.policy = 0         # index of neighbour to go to!
+        self.next_policy = 0
 
     def __str__(self):
         return 'state: ' + str(self.index) +\
                ', value: ' + str(self.value) +\
                ', next: ' + str(self.neighbours[self.policy])
 
+    def update_value(self):
+        is_change = self.value == self.next_value
+        self.value = self.next_value
+        return is_change
 
-def value_maze_from_states(states, n, m):
-    vm = np.zeros((n, m))
+    def update_policy(self):
+        is_change = self.policy == self.next_policy     # check convergence
+        self.policy = self.next_policy              # update policy
+        return is_change
+
+
+def get_state_values(states, n, m):
+    vm = np.full((n, m), np.nan)
     for state in states:
         vm[state.index] = state.value
     return vm
 
 
-def policy_maze_from_states(states):
+def show_state_policies(states):
     for state in states:
         other_state = state.neighbours[state.policy]
         x = state.index[1]
@@ -40,8 +52,6 @@ def policy_maze_from_states(states):
         y = -state.index[0]
         dy = -other_state.index[0] - y
         plt.arrow(x, y, dx, dy, fc='k', ec='k', head_width=0.15, head_length=0.4, length_includes_head=True)
-    plt.gca().set_aspect('equal')
-    plt.axis([-1, 7, -6, 1])
 
 
 states = []
@@ -53,6 +63,7 @@ for row in range(n_rows):
                 states.append(target)
             else:
                 states.append(State(index=(row, col)))
+
 n_states = len(states)
 
 for state in states:
@@ -72,31 +83,59 @@ for state in states:
 transition_reward = np.zeros((n_states, n_states))
 nes = target.neighbours
 for ne in nes:
-    transition_reward[states.index(ne), states.index(target)] = 10   # reward for going to / staying at the target!
+    transition_reward[states.index(ne), states.index(target)] = 1   # reward for going to / staying at the target!
 
-gamma = 0.99
+gamma = 0.1
 
-n_iter = 10
-fig, ax = plt.subplots(1, n_iter)
-fig1 = plt.figure(1)
+policy_convergence = np.full(n_states, False)
+value_convergence = np.full(n_states, False)
 
-for i in range(n_iter):
+i = 0
+while not (all(policy_convergence) and all(value_convergence)):
+    print('i={:.0f}'.format(i))
     " VALUE UPDATE "
     for state in states:
         value_elements = [transition_reward[states.index(state), states.index(ne)] + gamma*ne.value for ne in state.neighbours]
-        state.value = sum(value_elements)
+        state.next_value = sum(value_elements)
+    for state in states:
+        value_convergence[states.index(state)] = state.update_value()
+    if all(value_convergence):
+        print('\tvalues: converged')
+    else:
+        print('\tvalues: not converged')
 
     " POLICY UPDATE "
     for state in states:
         value_elements = [transition_reward[states.index(state), states.index(ne)] + gamma*ne.value for ne in state.neighbours]
-        state.policy = np.argmax(array([value_elements]))
+        state.next_policy = np.argmax(array([value_elements]))
+    for state in states:
+        policy_convergence[states.index(state)] = state.update_policy()
+    if all(policy_convergence):
+        print('\tpolicies: converged')
+    else:
+        print('\tpolicies: not converged')
 
-    # print(value_maze_from_states(states, n_rows, n_cols))
-    # ax[i].matshow(value_maze_from_states(states, n_rows, n_cols))
+    # plt.subplot(2, 30, i+1)
+    # plt.matshow(get_state_values(states, n_rows, n_cols), fignum=False)
+    #
+    # plt.subplot(2, 30, 30+i+1)
+    # show_state_policies(states)
+    # plt.gca().set_aspect('equal')
+    # plt.axis([-1, 7, -6, 1])
 
-    plt.figure(1)
-    plt.subplot(1, n_iter, i+1)
-    policy_maze_from_states(states)
+    i += 1
+
+values = get_state_values(states, n_rows, n_cols)
+print(values)
+
+plt.subplot(2, 1, 1)
+
+plt.matshow(np.log(values), fignum=False)
+
+plt.subplot(2, 1, 2)
+show_state_policies(states)
+plt.gca().set_aspect('equal')
+plt.axis([-1, 7, -6, 1])
 
 plt.show()
 
