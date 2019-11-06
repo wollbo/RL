8b5d958d6d8b6936cc51eda4b9123809fd691be1
torstenmolitor <torstenmolitor@hotmail.com>
2019-11-06 11:44:28 +0100

    finite horizon + infinite horizon

--- lab0_2.py
+++ lab0_2.py
@@ -15,10 +15,10 @@ weights = array([[0, 1, -np.inf, 10, 10, 10, 10],
 
 for t_horizon in range(38, 39):
     pass
-    # states = generate_maze_states(maze=maze, t_horizon=t_horizon, start=(0, 0))
-    # rewards = generate_transitions_rewards2(weights=weights, states=states)
-    # bi_values = backward_induction(maze=maze, states=states, rewards=rewards, t_horizon=t_horizon, plot=False, pause_time=0.1)
-    # plot_most_rewarding_path(State.start, values=bi_values, pause_time=0.01)
+    states = generate_maze_states(maze=maze, t_horizon=t_horizon, start=(0, 0))
+    rewards = generate_transitions_rewards2(weights=weights, states=states)
+    bi_values = backward_induction(maze=maze, states=states, rewards=rewards, t_horizon=t_horizon, plot=False, pause_time=0.1)
+    plot_most_rewarding_path(State.start, values=bi_values, pause_time=0.01)
 
 
 states = generate_maze_states2(maze=maze, start=(0, 0))
