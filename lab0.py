0696f19d44f29457a5213a0e630f9b8c583e2269
torstenmolitor <torstenmolitor@hotmail.com>
2019-11-07 18:40:33 +0100

    small fix

--- lab0.py
+++ lab0.py
@@ -6,27 +6,30 @@ from lab0_classes import *
 
 start_position = (0, 0)
 target_position = (5, 5)
-t_horizon = 12
-discount = 0.95
+r1_position = (5, 0)
+r2_position = (3, 6)
+time_horizon = 12
+discount = 0.9
 precision = 1
 maze = generate_maze()
 
-for _ in range(10):
-    states = generate_maze_states(maze=maze, t_horizon=t_horizon, start=start_position, target=target_position, r1=(5, 0), r2=(3, 6))
-    rewards = Reward(states, target=State.target, reward_target=3, reward_staying=-1, reward_moving=-1, r1=State.r1, r2=State.r2)
-    bi_values = backward_induction(maze=maze, states=states, t_horizon=t_horizon, rewards=rewards, plot=False, pause_time=0.1)
-    plot_most_rewarding_path(s0=State.start, values=bi_values, pause_time=0.1)
 
-# states = generate_maze_states2(maze=maze, start=start_position, target=target_position, discount=discount, precision=precision, r1=(5, 0), r2=(3, 6))
-# rewards = Reward(states, target=State2.target, reward_target=12, reward_staying=0, reward_moving=-1, r1=State2.r1, r2=State2.r2)
-# (vi_values, n) = value_iteration(maze=maze, states=states, rewards=rewards, plot=False, pause_time=0.1)
-# plot_most_rewarding_policy(states=states, values=vi_values, n=n, algorithm='Value Iteration')
+for _ in range(1):
+    states = StateSpace(maze=maze, time_horizon=time_horizon, start=start_position, target=target_position, r1=r1_position, r2=r2_position)
+    rewards = Reward(states, reward_target=3, reward_staying=-1, reward_moving=-1)
+    bi_values = backward_induction(maze=maze, states=states, rewards=rewards, plot=True, pause_time=0.1)
+    plot_most_rewarding_path(states=states, pause_time=0.1)
 
-for _ in range(10):
-    states = generate_maze_states2(maze=maze, start=start_position, target=target_position, discount=discount, precision=precision, r1=(5, 0), r2=(3, 6))
-    rewards = Reward(states, target=State2.target, reward_target=12, reward_staying=0, reward_moving=-1, r1=State2.r1, r2=State2.r2)
-    (pi_values, n) = policy_iteration(maze=maze, states=states, rewards=rewards, plot=False, pause_time=0.1)
-    plot_most_rewarding_policy(states=states, values=pi_values, n=n, algorithm='Policy Iteration')
+states = StateSpace2(maze=maze, discount=discount, precision=precision, start=start_position, target=target_position, r1=r1_position, r2=r2_position)
+rewards = Reward(states, reward_target=12, reward_staying=0, reward_moving=-1)
+(vi_values, n) = value_iteration(maze=maze, states=states, rewards=rewards, plot=True, pause_time=0.1)
+plot_most_rewarding_policy(states=states, n=n, algorithm='Value Iteration')
+
+states = StateSpace2(maze=maze, discount=discount, precision=precision, start=start_position, target=target_position, r1=r1_position, r2=r2_position)
+rewards = Reward(states, reward_target=12, reward_staying=0, reward_moving=-1)
+(pi_values, n) = policy_iteration(maze=maze, states=states, rewards=rewards, plot=True, pause_time=0.1)
+plot_most_rewarding_policy(states=states, n=n, algorithm='Policy Iteration')
+
 
 # print('Backwards Induction solution')
 # print(bi_values)
--- lab0_2.py
+++ lab0_2.py
@@ -1,4 +1,5 @@
 from lab0_functions import *
+from lab0_classes import *
 
 " PROBLEM 2 "
 
@@ -7,7 +8,9 @@ maze = np.full((6, 7), True)
 
 start_position = (0, 0)
 target_position = (5, 5)
-t_horizon = 12
+r1_position = (5, 0)
+r2_position = (3, 6)
+time_horizon = 40
 discount = 0.99
 precision = 0.3
 
@@ -18,28 +21,29 @@ weights = array([[0, 1, -np.inf, 10, 10, 10, 10],
                  [0, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf, 10],
                  [0, 0, 0, 0, 0, 11, 10]])
 
-#
-# for t_horizon in range(38, 39):
-#     states = generate_maze_states(maze=maze, t_horizon=t_horizon, start=(0, 0))
-#     rewards = Reward2(weights=weights, states=states)
-#     bi_values = backward_induction(maze=maze, states=states, rewards=rewards, t_horizon=t_horizon, plot=False, pause_time=0.5)
-#     plot_most_rewarding_path(State.start, values=bi_values, pause_time=0.01)
 
-states = generate_maze_states2(maze=maze, start=start_position, target=target_position, discount=discount, precision=precision)
+# for t_horizon in range(38, 39):
+states = StateSpace(maze=maze, time_horizon=time_horizon, start=start_position)
+rewards = Reward2(weights=weights, states=states)
+bi_values = backward_induction(maze=maze, states=states, rewards=rewards, plot=False, pause_time=0.5)
+plot_most_rewarding_path(states=states, pause_time=0.01)
+
+states = StateSpace2(maze=maze, precision=precision, discount=discount, start=start_position)
 rewards = Reward2(weights=weights, states=states)
 (vi_values, n_iterations) = value_iteration(maze=maze, states=states, rewards=rewards, plot=False, pause_time=0.1)
-plot_most_rewarding_policy(states=states, values=vi_values, n=n_iterations, algorithm='Value Iteration')
-#
-# states = generate_maze_states2(maze=maze, start=start_position, target=target_position, discount=discount, precision=precision)
-# rewards = Reward2(weights=weights, states=states)
-# (pi_values, n_iterations) = policy_iteration(maze=maze, states=states, rewards=rewards, plot=False, pause_time=0.1)
-# plot_most_rewarding_policy(states=states, values=pi_values, n=n_iterations, algorithm='Policy Iteration')
+plot_most_rewarding_policy(states=states, n=n_iterations, algorithm='Value Iteration')
 
-print('Backwards Induction solution')
-print(bi_values)
-print('Value Iteration solution')
-print(vi_values)
-print('Policy Iteration solution')
-print(pi_values)
+states = StateSpace2(maze=maze, precision=precision, discount=discount, start=start_position)
+rewards = Reward2(weights=weights, states=states)
+(pi_values, n_iterations) = policy_iteration(maze=maze, states=states, rewards=rewards, plot=False, pause_time=0.1)
+plot_most_rewarding_policy(states=states, n=n_iterations, algorithm='Policy Iteration')
+
+
+# #print('Backwards Induction solution')
+# #print(bi_values)
+# print('Value Iteration solution')
+# print(vi_values)
+# print('Policy Iteration solution')
+# print(pi_values)
 
 plt.show()
