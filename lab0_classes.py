200ebe2dc37b03525c7e83fead28e0b79aa0b3e9
torstenmolitor <torstenmolitor@hotmail.com>
2019-11-07 18:39:44 +0100

    state space classes implemented

--- lab0_classes.py
+++ lab0_classes.py
@@ -1,33 +1,147 @@
 from numpy.random import choice
 import numpy as np
+from itertools import count
+from lab0_functions import plot_arrow
+
+class StateSpace:       # state space for finite horizon problems
+    def __init__(self, maze, time_horizon,
+                 start=(None, None), target=(None, None),
+                 r1=(None, None), r2=(None, None)):
+        (n_rows, n_cols) = maze.shape
+        self.maze_shape = (n_rows, n_cols)
+        self.time_horizon = time_horizon
+        self.states = []
+        self.state_count = count(0)     # itertools count
+
+        for row in range(n_rows):
+            for col in range(n_cols):
+                if maze[row, col]:
+                    state = State(id=next(self.state_count),
+                                  index=(row, col),
+                                  time_horizon=self.time_horizon)
+                    if (row, col) == start:
+                        self.start = state
+                    elif (row, col) == target:
+                        self.target = state
+                    elif (row, col) == r1:
+                        self.r1 = state
+                    elif (row, col) == r2:
+                        self.r2 = state
+                    self.states.append(state)
+
+        for state in self.states:
+            state.neighbours.append(state)  # s can go to s
+            for other_state in self.states:
+                if np.abs(other_state.index[0] - state.index[0]) == 1 and np.abs(
+                        other_state.index[1] - state.index[1]) == 0:
+                    state.neighbours.append(other_state)  # same row neighbours
+                elif np.abs(other_state.index[0] - state.index[0]) == 0 and np.abs(
+                        other_state.index[1] - state.index[1]) == 1:
+                    state.neighbours.append(other_state)  # same column neighbours
+
+    def __len__(self):
+        return len(self.states)
+
+    def __getitem__(self, index):
+        return self.states[index]
+
+    def values(self, t):
+        vm = np.full(self.maze_shape, np.nan)
+        for state in self.states:
+            vm[state.index] = state.value[t]
+        return vm
+
+    def show_policies(self, t, axis):
+        axis.clear()
+        axis.set_yticklabels([])
+        axis.set_xticklabels([])
+        for state in self.states:
+            other_state = state.neighbours[state.action[t]]
+            plot_arrow(state, other_state)
 
 
 class State:  # state for finite horizon problems
-    time_horizon = None
-    start = None
-    target = None
-    r1 = None
-    r2 = None
-
-    def __init__(self, index):
+    def __init__(self, id, index, time_horizon):
+        self.id = id
         self.index = index
         self.neighbours = []
-        self.value = np.zeros(self.time_horizon)
-        self.action = np.zeros(self.time_horizon, dtype=np.uint8)  # local index of best neighbour to go to at time t
+        self.value = np.zeros(time_horizon)
+        self.action = np.zeros(time_horizon, dtype=np.uint8)  # local index of best neighbour to go to at time t
 
     def __str__(self):
         return str(self.index)
 
 
-class State2:  # state for infinite horizon problems
-    precision = None  # epsilon
-    discount = None  # lambda
-    start = None
-    target = None
-    r1 = None
-    r2 = None
+class StateSpace2:      # state space for infinite horizon problems
+    def __init__(self, maze, precision, discount,
+                 start=(None, None), target=(None, None),
+                 r1=(None, None), r2=(None, None)):
+        (n_rows, n_cols) = maze.shape
+        self.maze_shape = (n_rows, n_cols)
+        self.states = []
+        self.precision = precision  # epsilon
+        self.discount = discount    # lambda
+        self.diff_tolerance = self.precision * (1 - self.discount) / self.discount  # delta
+        self.state_count = count(0)     # itertools count
 
-    def __init__(self, index):
+        for row in range(n_rows):
+            for col in range(n_cols):
+                if maze[row, col]:
+                    state = State2(id=next(self.state_count),
+                                   index=(row, col))
+                    if (row, col) == start:
+                        self.start = state
+                    elif (row, col) == target:
+                        self.target = state
+                    elif (row, col) == r1:
+                        self.r1 = state
+                    elif (row, col) == r2:
+                        self.r2 = state
+                    self.states.append(state)
+
+        for state in self.states:
+            state.neighbours.append(state)  # s can go to s
+            for other_state in self.states:
+                if np.abs(other_state.index[0] - state.index[0]) == 1 and np.abs(
+                        other_state.index[1] - state.index[1]) == 0:
+                    state.neighbours.append(other_state)  # same row neighbours
+                elif np.abs(other_state.index[0] - state.index[0]) == 0 and np.abs(
+                        other_state.index[1] - state.index[1]) == 1:
+                    state.neighbours.append(other_state)  # same column neighbours
+
+        self.iteration_difference = np.full(len(self), 1, dtype=np.float)
+
+    def __getitem__(self, index):
+        return self.states[index]
+
+    def __len__(self):
+        return len(self.states)
+
+    def update_values(self):
+        for state in self.states:
+            self.iteration_difference[state.id] = state.update_value()
+
+    def stopping_condition(self):
+        return all(np.less(self.iteration_difference, self.diff_tolerance))
+
+    def values(self):
+        vm = np.full(self.maze_shape, np.nan)
+        for state in self.states:
+            vm[state.index] = state.value
+        return vm
+
+    def show_policies(self, axis):
+        axis.clear()
+        axis.set_yticklabels([])
+        axis.set_xticklabels([])
+        for state in self.states:
+            other_state = state.neighbours[state.action]
+            plot_arrow(state, other_state)
+
+
+class State2:  # state for infinite horizon problems
+    def __init__(self, id, index):
+        self.id = id
         self.index = index
         self.neighbours = []
         self.value = 0
@@ -38,13 +152,10 @@ class State2:  # state for infinite horizon problems
     def __str__(self):
         return 'state: ' + str(self.index)
 
-    def value_tolerance(self):  # delta
-        return self.precision * (1 - self.discount) / self.discount
-
     def update_value(self):
-        error = np.abs(self.value - self.next_value)   # check convergence
+        difference = np.abs(self.value - self.next_value)  # check convergence
         self.value = self.next_value  # update value
-        return error
+        return difference
 
     def update_action(self):
         no_change = self.action == self.next_action  # check convergence
@@ -52,39 +163,42 @@ class State2:  # state for infinite horizon problems
         return no_change
 
 
-class Reward:
-    def __init__(self, states, target, reward_staying, reward_moving, reward_target, r1, r2):
+class Reward:       # shortest path rewards
+    def __init__(self, states, reward_staying, reward_moving, reward_target):
         self.n = len(states)
         self.matrix = np.full((self.n, self.n), reward_moving)
         self.deterministic = np.full((self.n, self.n), True)
-        self.to_r1 = []     # indices of transitions going to r1
-        self.to_r2 = []     # indices of transitions going to r2
+        self.to_r1 = []  # indices of transitions going to r1
+        self.to_r2 = []  # indices of transitions going to r2
 
         for i in range(self.n):
             self.matrix[i, i] = reward_staying
 
-        for ne in target.neighbours:
-            self.matrix[states.index(ne), states.index(target)] = reward_target
+        for ne in states.target.neighbours:
+            self.matrix[ne.id, states.target.id] = reward_target
 
-        for nr1 in r1.neighbours:
-            self.to_r1.append((states.index(nr1), states.index(r1)))
-            self.deterministic[states.index(nr1), states.index(r1)] = False
+        for nr1 in states.r1.neighbours:
+            self.to_r1.append((nr1.id, states.r1.id))
+            self.deterministic[nr1.id, states.r1.id] = False
 
-        for nr2 in r2.neighbours:
-            self.to_r2.append((states.index(nr2), states.index(r2)))
-            self.deterministic[states.index(nr2), states.index(r2)] = False
+        for nr2 in states.r2.neighbours:
+            self.to_r2.append((nr2.id, states.r2.id))
+            self.deterministic[nr2.id, states.r2.id] = False
 
     def __getitem__(self, indices):
         if self.deterministic[indices]:
             return self.matrix[indices]
         else:
             if indices in self.to_r1:
-                return choice([-1, -70], 1, p=[0.2, 0.8])[0]
+                return choice([-1, -7], 1, p=[0.5, 0.5])[0]
             elif indices in self.to_r2:
                 return choice([-1, -2], 1, p=[0.5, 0.5])[0]
 
+    def __str__(self):
+        return str(self.matrix)
 
-class Reward2:
+
+class Reward2:      # plucking berries
     def __init__(self, states, weights):
         self.n = len(states)
         self.matrix = np.full((self.n, self.n), 0, dtype=np.float)
@@ -92,11 +206,10 @@ class Reward2:
         for state in states:
             for ne in state.neighbours:
                 if ne != state:
-                    self.matrix[states.index(ne), states.index(state)] = weights[state.index]
+                    self.matrix[ne.id, state.id] = weights[state.index]
 
     def __getitem__(self, indices):
         return self.matrix[indices]
 
     def __str__(self):
         return str(self.matrix)
-
