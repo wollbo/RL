c6fbb8e5af6aaf4304484f61b00160be4fe07730
torstenmolitor <torstenmolitor@hotmail.com>
2019-11-22 23:49:47 +0100

    terminal state added

--- lab1_1.py
+++ lab1_1.py
@@ -9,33 +9,28 @@ exit = (6, 5)
 
 rewards = Reward(reward_eaten=-1, reward_exiting=1)
 
-# states = StateSpace(mdp='finite',
-#                     maze=maze,
-#                     time_horizon=time_horizon,
-#                     entry=entry, exit=exit,
-#                     minotaur_can_stand_still=False)
-# backward_induction(states=states, rewards=rewards)
-#
-# run_game(states=states, rewards=rewards)
+states = StateSpace(mdp='finite',
+                    maze=maze,
+                    time_horizon=time_horizon,
+                    entry=entry, exit=exit,
+                    minotaur_can_stand_still=True)
+backward_induction(states=states, rewards=rewards)
+
+run_game(states=states, rewards=rewards)
 
 
 states = StateSpace(mdp='discounted',
                     maze=maze,
                     discount_factor=29/30,
-                    precision=1,
+                    precision=0.1,
                     entry=entry, exit=exit,
                     minotaur_can_stand_still=True)
 value_iteration(states=states, rewards=rewards, plot=False)
 
-while True:
-    run_game(states=states, rewards=rewards)
+run_game(states, rewards)
 
-# pe = probability_of_exiting(states, 10000)
-# print('probability of exiting: {:.6f}'.format(pe))
-
-# show_values(states=states, maze=maze, minotaur_position=(4, 5))
-# show_policies(states=states, maze=maze, minotaur_position=(4, 5))
-
+show_values(states=states, maze=maze, minotaur_position=(4, 5))
+show_policies(states=states, maze=maze, minotaur_position=(4, 5))
 
 plt.show()
 
