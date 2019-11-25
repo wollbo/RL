08b115a2db4166c4555129220060106932795774
torstenmolitor <torstenmolitor@hotmail.com>
2019-11-25 19:53:02 +0100

    test for log spaced samples

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-H = 6
-L = 7
+t_log = []
 
-bMat = np.zeros([H,L])
-pMat = bMat
+for t in range(1, 1000):
+    if round(1000 * np.log10(t)) % 1000 == 0:
+        print(t)
+        print(np.log10(t))
+        t_log.append(t)
+
 
 
 
